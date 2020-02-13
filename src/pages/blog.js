7077e02be575b29201c35f3b46c17543f455338f
Mitchell Redekopp <mitch32@gmail.com>
2020-02-13 11:15:45 -0600

    initial commit

---
+++ src/pages/blog.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default function blog() {
+  return (
+    <div>
+      Hello From Blog Page! <Link to="/">Home Page</Link>
+    </div>
+  )
+}
---
+++ src/pages/tours.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function tours() {
+  return <div>Hello from Tours Page!!</div>
+}
